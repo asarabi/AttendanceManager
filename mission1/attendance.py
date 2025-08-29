fe0b6c5199ad57454ee9a33c83dc390ba055865a
changekun im <asarabi8282@gmail.com>
2025-08-29 10:45:04 +0900

    [refact] extract method some function
    extract method some function.
    
    Signed-off-by: changekun im <asarabi8282@gmail.com>

--- mission1/attendance.py
+++ mission1/attendance.py
@@ -2,22 +2,17 @@ unique_uniform_num = {}
 uniform_num_count = 0
 
 # dat[사용자ID][요일]
-data_by_day = [[0] * 100 for _ in range(100)]
+attendance_by_day = [[0] * 100 for _ in range(100)]
 points = [0] * 100
 grade = [0] * 100
 player_names = [''] * 100
-wed = [0] * 100
-weekend = [0] * 100
+wednesday_attendance = [0] * 100
+weekend_attendance = [0] * 100
 
 def calc_attendance_point(attendee_name, attendance_date):
     global uniform_num_count
 
-    if attendee_name not in unique_uniform_num:
-        uniform_num_count += 1
-        unique_uniform_num[attendee_name] = uniform_num_count
-        player_names[uniform_num_count] = attendee_name
-
-    uniform_num = unique_uniform_num[attendee_name]
+    uniform_num = get_uniform_num(attendee_name)
 
     add_point = 0
     day_index = 0
@@ -31,7 +26,7 @@ def calc_attendance_point(attendee_name, attendance_date):
     elif attendance_date == "wednesday":
         day_index = 2
         add_point += 3
-        wed[uniform_num] += 1
+        wednesday_attendance[uniform_num] += 1
     elif attendance_date == "thursday":
         day_index = 3
         add_point += 1
@@ -41,65 +36,89 @@ def calc_attendance_point(attendee_name, attendance_date):
     elif attendance_date == "saturday":
         day_index = 5
         add_point += 2
-        weekend[uniform_num] += 1
+        weekend_attendance[uniform_num] += 1
     elif attendance_date == "sunday":
         day_index = 6
         add_point += 2
-        weekend[uniform_num] += 1
+        weekend_attendance[uniform_num] += 1
 
-    data_by_day[uniform_num][day_index] += 1
+    attendance_by_day[uniform_num][day_index] += 1
     points[uniform_num] += add_point
 
 
+def get_uniform_num(attendee_name):
+    global uniform_num_count
+    if attendee_name not in unique_uniform_num:
+        uniform_num_count += 1
+        unique_uniform_num[attendee_name] = uniform_num_count
+        player_names[uniform_num_count] = attendee_name
+    uniform_num = unique_uniform_num[attendee_name]
+    return uniform_num
+
+
 def get_attendance_data():
-    with open("attendance_weekday_500.txt", encoding='utf-8') as f:
-        lines_data = []
-        for _ in range(500):
-            line = f.readline()
-            if not line:
-                break
-            lines_data.append(line.strip().split())
+    try:
+        with open("attendance_weekday_500.txt", encoding='utf-8') as f:
+            lines_data = []
+            for _ in range(500):
+                line = f.readline()
+                if not line:
+                    break
+                lines_data.append(line.strip().split())
+    except FileNotFoundError:
+        print("파일을 찾을 수 없습니다.")
+
     return lines_data
 
 
 def get_eliminate_list():
-    try:
-        attendance_data = get_attendance_data()
 
-        for attendee_info in attendance_data:
-            if len(attendee_info) == 2:
-                calc_attendance_point(attendee_info[0], attendee_info[1])
+    attendance_data = get_attendance_data()
 
-        for i in range(1, uniform_num_count + 1):
-            if data_by_day[i][2] > 9:
-                points[i] += 10
-            if data_by_day[i][5] + data_by_day[i][6] > 9:
-                points[i] += 10
+    for attendee_info in attendance_data:
+        if len(attendee_info) == 2:
+            calc_attendance_point(attendee_info[0], attendee_info[1])
 
-            if points[i] >= 50:
-                grade[i] = 1
-            elif points[i] >= 30:
-                grade[i] = 2
-            else:
-                grade[i] = 0
+    for attendee in range(1, uniform_num_count + 1):
+        get_bonus_points(attendee)
+        calc_grade(attendee)
+        print_player_data(attendee)
 
-            print(f"NAME : {player_names[i]}, POINT : {points[i]}, GRADE : ", end="")
-            if grade[i] == 1:
-                print("GOLD")
-            elif grade[i] == 2:
-                print("SILVER")
-            else:
-                print("NORMAL")
+    print_remove_player_list()
 
-        print("\nRemoved player")
-        print("==============")
-        for i in range(1, uniform_num_count + 1):
-            if grade[i] not in (1, 2) and wed[i] == 0 and weekend[i] == 0:
-                print(player_names[i])
 
-    except FileNotFoundError:
-        print("파일을 찾을 수 없습니다.")
+def print_remove_player_list():
+    print("\nRemoved player")
+    print("==============")
+    for attendee in range(1, uniform_num_count + 1):
+        if grade[attendee] not in (1, 2) and wednesday_attendance[attendee] == 0 and weekend_attendance[attendee] == 0:
+            print(player_names[attendee])
 
+
+def print_player_data(attendee):
+    print(f"NAME : {player_names[attendee]}, POINT : {points[attendee]}, GRADE : ", end="")
+    if grade[attendee] == 1:
+        print("GOLD")
+    elif grade[attendee] == 2:
+        print("SILVER")
+    else:
+        print("NORMAL")
+
+
+def calc_grade(attendee):
+    if points[attendee] >= 50:
+        grade[attendee] = 1
+    elif points[attendee] >= 30:
+        grade[attendee] = 2
+    else:
+        grade[attendee] = 0
+
+
+def get_bonus_points(attendee):
+    if attendance_by_day[attendee][2] > 9:
+        points[attendee] += 10
+    if attendance_by_day[attendee][5] + attendance_by_day[attendee][6] > 9:
+        points[attendee] += 10
 
 
 if __name__ == "__main__":
