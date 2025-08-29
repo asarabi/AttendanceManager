318e9429950119bb6ae2e9033486756469caeb66
changekun im <asarabi8282@gmail.com>
2025-08-29 09:52:40 +0900

    [refact] mission1: change variable name
    To improve redability, change variable name
    
    Signed-off-by: changekun im <asarabi8282@gmail.com>

--- mission1/attendance.py
+++ mission1/attendance.py
@@ -1,70 +1,70 @@
-id1 = {}
-id_cnt = 0
+unique_uniform_num = {}
+uniform_num_count = 0
 
 # dat[사용자ID][요일]
-dat = [[0] * 100 for _ in range(100)]
+data_by_day = [[0] * 100 for _ in range(100)]
 points = [0] * 100
 grade = [0] * 100
-names = [''] * 100
+player_names = [''] * 100
 wed = [0] * 100
-weeken = [0] * 100
+weekend = [0] * 100
 
-def input2(w, wk):
-    global id_cnt
+def calc_attendance_point(attendee_name, attendance_date):
+    global uniform_num_count
 
-    if w not in id1:
-        id_cnt += 1
-        id1[w] = id_cnt
-        names[id_cnt] = w
+    if attendee_name not in unique_uniform_num:
+        uniform_num_count += 1
+        unique_uniform_num[attendee_name] = uniform_num_count
+        player_names[uniform_num_count] = attendee_name
 
-    id2 = id1[w]
+    uniform_num = unique_uniform_num[attendee_name]
 
     add_point = 0
-    index = 0
+    day_index = 0
 
-    if wk == "monday":
-        index = 0
+    if attendance_date == "monday":
+        day_index = 0
         add_point += 1
-    elif wk == "tuesday":
-        index = 1
+    elif attendance_date == "tuesday":
+        day_index = 1
         add_point += 1
-    elif wk == "wednesday":
-        index = 2
+    elif attendance_date == "wednesday":
+        day_index = 2
         add_point += 3
-        wed[id2] += 1
-    elif wk == "thursday":
-        index = 3
+        wed[uniform_num] += 1
+    elif attendance_date == "thursday":
+        day_index = 3
         add_point += 1
-    elif wk == "friday":
-        index = 4
+    elif attendance_date == "friday":
+        day_index = 4
         add_point += 1
-    elif wk == "saturday":
-        index = 5
+    elif attendance_date == "saturday":
+        day_index = 5
         add_point += 2
-        weeken[id2] += 1
-    elif wk == "sunday":
-        index = 6
+        weekend[uniform_num] += 1
+    elif attendance_date == "sunday":
+        day_index = 6
         add_point += 2
-        weeken[id2] += 1
+        weekend[uniform_num] += 1
 
-    dat[id2][index] += 1
-    points[id2] += add_point
+    data_by_day[uniform_num][day_index] += 1
+    points[uniform_num] += add_point
 
-def input_file():
+def get_eliminate_list():
     try:
         with open("attendance_weekday_500.txt", encoding='utf-8') as f:
             for _ in range(500):
                 line = f.readline()
                 if not line:
                     break
-                parts = line.strip().split()
-                if len(parts) == 2:
-                    input2(parts[0], parts[1])
+                attendee_info = line.strip().split()
+                if len(attendee_info) == 2:
+                    calc_attendance_point(attendee_info[0], attendee_info[1])
 
-        for i in range(1, id_cnt + 1):
-            if dat[i][2] > 9:
+        for i in range(1, uniform_num_count + 1):
+            if data_by_day[i][2] > 9:
                 points[i] += 10
-            if dat[i][5] + dat[i][6] > 9:
+            if data_by_day[i][5] + data_by_day[i][6] > 9:
                 points[i] += 10
 
             if points[i] >= 50:
@@ -74,7 +74,7 @@ def input_file():
             else:
                 grade[i] = 0
 
-            print(f"NAME : {names[i]}, POINT : {points[i]}, GRADE : ", end="")
+            print(f"NAME : {player_names[i]}, POINT : {points[i]}, GRADE : ", end="")
             if grade[i] == 1:
                 print("GOLD")
             elif grade[i] == 2:
@@ -84,12 +84,12 @@ def input_file():
 
         print("\nRemoved player")
         print("==============")
-        for i in range(1, id_cnt + 1):
-            if grade[i] not in (1, 2) and wed[i] == 0 and weeken[i] == 0:
-                print(names[i])
+        for i in range(1, uniform_num_count + 1):
+            if grade[i] not in (1, 2) and wed[i] == 0 and weekend[i] == 0:
+                print(player_names[i])
 
     except FileNotFoundError:
         print("파일을 찾을 수 없습니다.")
 
 if __name__ == "__main__":
-    input_file()
+    get_eliminate_list()
